724d1fa2cd8e460e87defa74457785855832329b
sairaman2112 <sairamankoraveni21@gmail.com>
2020-12-13 18:45:00 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Footer from './Components/Footer';
 // import HomePage from './Components/HomePage';
 import Navbar from './Components/Navbar';
+import SignIn from './Components/SignIn';
+
+import Register from './Components/Register';
 import Title from './Components/Title';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Title />
-      <Footer />
+      
     </div>
   )
   
